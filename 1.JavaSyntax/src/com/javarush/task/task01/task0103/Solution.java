f7b4595a5991c2fe6380351cf1b13bcb92ace1f2
legg78 <tula78@mail.ru>
2017-12-26 00:12:29 +0300

    JavaSyntax0103

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task01.task0103;
+
+/* 
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        final Integer year = 3126;
+        final Integer age = 8;
+        final Integer yearBD = year - age;
+        System.out.println(yearBD.toString());
+    }
+}
