fe78f6c9538bf1ee7d5ac39f83c33b4089c3fd35
legg78 <tula78@mail.ru>
2017-12-26 00:01:21 +0300

    Initial commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0102/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0102;
+
+/* 
+Комплимент учителю
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (int ii=0; ii++<5;)
+            System.out.println("Элли самая красивая");
+    }
+}
